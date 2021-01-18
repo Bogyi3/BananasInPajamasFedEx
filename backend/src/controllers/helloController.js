208146e59da801dcd30f2725ae69d23903506652
Boglarka Nagy <n.boglar3@gmail.com>
2021-01-18 12:29:44 +0100

    initial commit, setting up environment

---
+++ backend/src/controllers/helloController.js
@@ -0,0 +1,8 @@
+import { helloService } from '../services';
+
+export const helloController = {
+  async get(req, res) {
+    const data = await helloService.getHelloWorld();
+    res.status(200).json(data);
+  },
+};
