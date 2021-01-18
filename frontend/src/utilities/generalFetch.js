1b401c2285b6c3f623ebb2ddfbc5a2eb01b9e8af
Boglarka Nagy <n.boglar3@gmail.com>
2021-01-18 19:20:16 +0100

    added registration component logic TODO: create stylig

---
+++ frontend/src/utilities/generalFetch.js
@@ -0,0 +1,22 @@
+async function generalFetch(endpoint, method, bodyData = undefined, token = undefined) {
+  const requestOptions = {
+    method,
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  };
+
+  if (endpoint !== 'sessions' && endpoint !== 'registration') {
+    requestOptions.headers.Authorization = `Bearer ${token}`;
+  }
+
+  if (bodyData !== undefined) { requestOptions.body = JSON.stringify(bodyData); }
+  const httpResponse = await fetch(`${process.env.REACT_APP_API_URL}/${endpoint}`, requestOptions);
+  const response = await httpResponse.json();
+  const { status } = httpResponse;
+
+  const data = { response, status };
+  return data;
+}
+export default generalFetch;
