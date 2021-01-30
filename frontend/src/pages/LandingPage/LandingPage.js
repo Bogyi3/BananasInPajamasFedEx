52701903a9b19cc495046d4d2247232215a763c4
Sisa <tamas.sisa@gmail.com>
2021-01-30 13:29:41 +0100

    literally one line

--- frontend/src/pages/LandingPage/LandingPage.js
+++ frontend/src/pages/LandingPage/LandingPage.js
@@ -16,26 +16,27 @@ function LandingPage() {
 
   return (
     <div>
-      {!loginData.username && (
-      <div className="landingPage page">
-        <Typography color="secondary" variant="h1">Welcome to Resolute!</Typography>
-        <div className="instruction">
-          <Typography color="secondary" variant="h3">Please, </Typography>
-          <Link to="/login" style={{ textDecoration: 'none' }}>
-            <Button variant="contained" color="primary" size="small">
-              SIGN IN
-            </Button>
-          </Link>
-          <Typography color="secondary" variant="h3">or</Typography>
-          <Link to="/registration" style={{ textDecoration: 'none' }}>
-            <Button variant="contained" color="primary" size="medium">
-              REGISTER
-            </Button>
-          </Link>
-          <Typography color="secondary" variant="h3">to continue.</Typography>
+      {!loginData.username ? (
+        <div className="landingPage page">
+          <Typography color="secondary" variant="h1">Welcome to Resolute!</Typography>
+          <div className="instruction">
+            <Typography color="secondary" variant="h3">Please, </Typography>
+            <Link to="/login" style={{ textDecoration: 'none' }}>
+              <Button variant="contained" color="primary" size="small">
+                SIGN IN
+              </Button>
+            </Link>
+            <Typography color="secondary" variant="h3">or</Typography>
+            <Link to="/registration" style={{ textDecoration: 'none' }}>
+              <Button variant="contained" color="primary" size="medium">
+                REGISTER
+              </Button>
+            </Link>
+            <Typography color="secondary" variant="h3">to continue.</Typography>
+          </div>
         </div>
-      </div>
-      )}
+      )
+        : <div>Current challenge:</div>}
     </div>
   );
 }
